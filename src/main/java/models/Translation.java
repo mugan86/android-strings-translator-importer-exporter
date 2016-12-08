ddac3514ea9d4d75b3f61511d8038417ecaf42ee
anartzmugika <mugan86@gmail.com>
2016-12-08 12:00:28 +0100

    Update Gitignore file

---
+++ src/main/java/models/Translation.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by anartzmugika on 8/12/16.
+ */
+public class Translation {
+}
---
+++ src/main/java/utils/InfoManage.java
@@ -0,0 +1,7 @@
+package utils;
+
+/**
+ * Created by anartzmugika on 8/12/16.
+ */
+public class InfoManage {
+}
