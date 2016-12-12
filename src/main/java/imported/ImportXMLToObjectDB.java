da95c6e9a1156a0fe847865888d6d59207063486
Anartz Muxika Ledo <mugan86@gmail.com>
2016-12-12 12:52:40 +0100

    Execute data and import from xml

--- src/main/java/imported/ImportXMLToObjectDB.java
+++ src/main/java/imported/ImportXMLToObjectDB.java
@@ -2,10 +2,9 @@ package imported;
 
 import db.DBManage;
 import model.Translation;
+import utils.ManageFile;
 import values.ConstantValues;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
 /***********************************************************************************************************************
@@ -13,39 +12,21 @@ import java.util.ArrayList;
  **********************************************************************************************************************/
 public class ImportXMLToObjectDB {
     private ArrayList<Translation> translations;
-    private DBManage info_manage;
-    public static void main(String[] args) throws IOException {
-
-        String fileName = "en/strings.xml";
-        //new ImportXMLToObjectDB().readFileResources();
-        new ImportXMLToObjectDB().readFileResources(fileName, false);
-
-    }
-
-    private File getFile(String fileName)
-    {
-        // Getting ClassLoader obj
-        ClassLoader classLoader = getClass().getClassLoader();
-        // Getting resource(File) from class loader
-        return new File(classLoader.getResource(fileName).getFile());
-    }
-
-    private void readFileResources(String fileName, boolean add_data_in_db)
+    public void readFileResources(String fileName, boolean add_data_in_db)
     {
         translations = new ArrayList<Translation>();
 
         DBManage info_manage = new DBManage(ConstantValues.DEFAULT_DB_PATH);
-        //info_manage.getRegisterCount();
         info_manage.setUselanguage(fileName);
 
         if(add_data_in_db) //Add
         {
-            new Translation().readXMLFile(getFile(fileName), translations, info_manage.getSelect_language());
+            ManageFile manageFile = new ManageFile();
+            manageFile.readXMLFile(manageFile.getFile(fileName), translations, info_manage.getSelect_language());
             info_manage.addTranslationsInSelectLanguage(translations);
         }
 
         info_manage.showAllTranslationsRegisters();
         info_manage.closeDataBase();
     }
-
 }
