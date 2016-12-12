c91004ce7b077452812a40a062159839f7f6434f
Anartz Muxika Ledo <mugan86@gmail.com>
2016-12-12 12:51:15 +0100

    Remove readXMLFile

--- src/main/java/model/Translation.java
+++ src/main/java/model/Translation.java
@@ -151,41 +151,4 @@ public class Translation implements Serializable {
     {
         return this.getName() + ": " + this.getText_eu()  + " / " + this.getText_es() + " / " + this.getText_ca()+ " / Select Language: " + this.getSelect_language();
     }
-
-    public ArrayList<Translation> readXMLFile(File file, ArrayList<Translation> translations, String select_language)
-    {
-        DocumentBuilderFactory docFactory= DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder;
-        try {
-            docBuilder = docFactory.newDocumentBuilder();
-            Document doc=docBuilder.parse(file);
-
-
-            NodeList strings= doc.getElementsByTagName("string");
-            System.out.println(strings.getLength());
-            System.out.println("***************************************");
-            for(int i = 0; i < strings.getLength(); i++)
-            {
-                Node node_str_element= strings.item(i);
-                //Erosle bakoitzaren informazioa eman ahal izateko luzeraren arabera
-                Element str_element = (Element)node_str_element;
-                String name = str_element.getAttribute("name");
-                String text = str_element.getTextContent();
-                //Elementuaren id atributua hartu eta bistaratuko du
-                System.out.println("name: "+ name);
-                System.out.println("Text: "+ text);
-                System.out.println("***************************************");
-
-
-                translations.add(new Translation(name, text, select_language));
-            }
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return translations;
-    }
 }
