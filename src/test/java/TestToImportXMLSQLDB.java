05d3f22c08cd4b0b5ff0f48698cc7c727a511a43
anartzmugika <mugan86@gmail.com>
2016-12-12 20:23:25 +0100

    Execute import xml to sql

--- src/test/java/TestToImportXMLSQLDB.java
+++ src/test/java/TestToImportXMLSQLDB.java
@@ -1,15 +1,15 @@
-import imported.ImportXMLToObjectDB;
+import imported.ImportXMLToSQL;
 
 /***********************************************************************************************************************
  * Created by anartzmugika on 12/12/16.
  * --------------------------------------
  * Test main to import xml (strings.xml) from resources to make operation (read only or read and add in database data)
  ***********************************************************************************************************************/
-public class TestToImportXMLToObjectDB {
+public class TestToImportXMLSQLDB {
     public static void main(String[] args)
     {
         String fileName = "en/strings.xml";
         //new ImportXMLToObjectDB().readFileResources();
-        new ImportXMLToObjectDB().readFileResources(fileName, false);
+        new ImportXMLToSQL().readFileResources(fileName, true);
     }
 }
