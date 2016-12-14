c9a70948a3e4c82920debb4ab1ff9827710fa988
anartzmugika <mugan86@gmail.com>
2016-12-14 20:20:58 +0100

    Add new constant with relation to db

--- src/main/java/values/ConstantValues.java
+++ src/main/java/values/ConstantValues.java
@@ -15,12 +15,42 @@ public class ConstantValues {
     public static final String GET_INFO_IN_SELECT_LANGUAGE = "SELECT p FROM Translation p WHERE text_%s != null";
 
     //SQL Import/Export constants
-    public static final String CREATE_DATABASE = "CREATE DATABASE %s DEFAULT CHARACTER SET utf8 " +
+    public static final String CREATE_DATABASE = "CREATE DATABASE IF NOT EXISTS %s DEFAULT CHARACTER SET utf8 " +
                                                                     "  DEFAULT COLLATE utf8_general_ci;";
-    public static final String CREATE_TRANSLATION_TABLE = "CREATE TABLE IF NOT EXISTS translations ( name VARCHAR(100) PRIMARY KEY, text_eu TEXT,  text_en TEXT, text_es TEXT, text_ca TEXT,  text_ga TEXT, text_pt TEXT, text_it TEXT);";
 
-    public static final String ADD_TRANSLATION_IF_NOT_EXISTS = "INSERT IGNORE INTO `translations` (`id`, `name`, `text_eu`, `text_en`, `text_es`, `text_ca`, `text_ga`, `text_pt`, `text_it`)" +
-                                                                "VALUES (NULL, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');";
+    public static final String CREATE_TRANSLATION_TABLE = "CREATE TABLE IF NOT EXISTS `translations` (\n" +
+            "  `id` int(11) NOT NULL,\n" +
+            "  `name` varchar(100) NOT NULL,\n" +
+            "  `text_eu` text,\n" +
+            "  `text_en` text,\n" +
+            "  `text_es` text,\n" +
+            "  `text_ca` text,\n" +
+            "  `text_ga` text,\n" +
+            "  `text_pt` text,\n" +
+            "  `text_it` text,\n" +
+            "  `app_id` varchar(200) NOT NULL,\n" +
+            "  PRIMARY KEY (`name`,`app_id`),\n" +
+            "  KEY `app_id` (`app_id`)\n" +
+            ") ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+    public static final String CREATE_APP_TO_TRANSLATE = "CREATE TABLE IF NOT EXISTS `app` (\n" +
+                                                                            "  `id` varchar(200) NOT NULL,\n" +
+                                                                            "  `name` varchar(200) NOT NULL,\n" +
+                                                                            "  `description` text NOT NULL,\n" +
+                                                                            "  `first_published` date NOT NULL,\n" +
+                                                                            "  `last_update` date NOT NULL,\n" +
+                                                                            "  `original_language` varchar(20) NOT NULL,\n" +
+                                                                            "  PRIMARY KEY (`id`)\n" +
+                                                                            ") ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+    public static final String CREATE_APP_TRANSLATION_RELATION = "ALTER TABLE `translations`\n" +
+            "  ADD CONSTRAINT `translations_ibfk_1` FOREIGN KEY (`app_id`) REFERENCES `app` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;";
+
+    public static final String ADD_APP_INFO_IF_NOT_EXISTS = "INSERT IGNORE INTO `app` (`id`, `name`, `description`, `first_published`, `last_update`, `original_language`)" +
+            "VALUES ('%s', '%s', '%s', '%s', '%s', '%s');";
+
+    public static final String ADD_TRANSLATION_IF_NOT_EXISTS = "INSERT IGNORE INTO `translations` (`id`, `name`, `text_eu`, `text_en`, `text_es`, `text_ca`, `text_ga`, `text_pt`, `text_it`, `app_id`)" +
+                                                                "VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');";
     //Messages
     public static final String DELETE_ROWS_MSG = "%d registers delete!!!";
 }
